3b59f33d62743319163acf03e221841e57f44df6
Molnár Bence <molnar.bence.email@gmail.com>
2024-05-30 09:12:04 +0200

    Szerkesztve 3

---
+++ src/app.ts
@@ -0,0 +1,53 @@
+class Romb {
+    //kötések (bind) pipe [pájp] AltGr + w = |
+    sideInput?: HTMLInputElement | null;
+    perimeterInput?: HTMLInputElement | null;
+    areaInput?: HTMLInputElement | null;
+    calcButton?: HTMLButtonElement | null;
+
+
+    constructor() {
+        this.bindHtml();
+        this.handleEvent();
+    }   
+
+    bindHtml() {
+        this.sideInput = document.querySelector("#side");
+        this.perimeterInput = document.querySelector("#perimeter");
+        this.areaInput = document.querySelector("#area");
+        this.calcButton = document.querySelector("#calcButton");
+    }
+
+    handleEvent() {
+        this.calcButton?.addEventListener('click', () => {
+            this.startCalc();
+        });
+    }
+
+    startCalc() {
+        const side = Number(this.sideInput?.value);
+        const perimeter = Number(this.perimeterInput?.value);
+        const area = Number(this.areaInput?.value);
+        this.rederResult(perimeter, area);
+
+    }
+    calcPerimeter(side: number): number {
+        return side * 4;
+    }
+    calcArea(side: number, alpha: number): number {
+        const rad = alpha * Math.PI /180;
+        return Math.pow(side, 2) * Math.sin(rad);
+    }
+    rederResult(perimeter: number, area: number) {
+        if(this.perimeterInput) {
+        this.perimeterInput.value = String(perimeter);
+        }
+        if(this.areaInput) {
+            this.areaInput.value = String(area);
+        }
+    
+    }
+}
+
+
+new Romb();
